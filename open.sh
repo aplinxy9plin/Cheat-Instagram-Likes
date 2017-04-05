cd95d524135f55925b3fb99fa7fc5d8c55028968
Наташенька ❤️ <evilberry@icloud.com>
2017-04-05 23:58:13 +0700

    Update open.sh

--- open.sh
+++ open.sh
@@ -3,6 +3,7 @@ read -p "Введи ссылку на фото " test;
 echo To end press CTRL + C;
 echo '<?php $test = "'$test'" ?>'>> media.php;
 tor;
+php script2.php;
 while true; do
   php script.php
   echo "key";
